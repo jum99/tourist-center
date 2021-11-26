cf7724a1619ac9b6af1048a9e602b51d91555660
jum99 <ejumibegum@gmail.com>
2021-11-26 14:56:19 +0600

    last commit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,49 +10,49 @@ const Contact = () => {
                 <Col className="">
                     <div className=" p-3 rounded-3  contact-div">
                         <h4>POSTAL ADDRESS</h4>
-                        <p> Marriott, Locked Bag 1 A'Beckett St, Melbourne, VIC 8006</p>
+                        <p>204 Hamton Road, UK</p>
                     </div>
                 </Col>
                 <Col>
                     <div className=" p-3 rounded-3  contact-div">
                         <h4> Weekly Trip</h4>
-                        <p> Questions & enquiries </p>
-                        <h6>Phone(03) 8559 5330</h6>
+                        <p>Support Number:</p>
+                        <h6>(809) 0876 964</h6>
                     </div>
                 </Col>
                 <Col>
                     <div className=" p-3 rounded-3  contact-div">
                         <h4> NAVIGATORS</h4>
                         <p>
-                            For help with support or getting in touch with your team, contact
-                            your tour navigator Navigator
+                            For help with support or getting in touch with our team, contact
+                            our tour navigators.
                         </p>
-                        <h6>Phone(03) 8559 5000</h6>
+                        <h6>Phone: (036) 7121 5548</h6>
                     </div>
                 </Col>
                 <Col>
                     <div className=" p-3 rounded-3  contact-div">
                         <h4> URGENT BOOKING</h4>
-                        <p> QUESTION & enquiries</p>
-                        <h6>Phone(03) 8559 6000</h6>
+                        <p>Call here:</p>
+                        <h6>(099) 8865 6670</h6>
                     </div>
                 </Col>
                 <Col>
                     <div className=" p-3 rounded-3 contact-div">
                         <h4> REFERRALS</h4>
-                        <p>Business hours, Mon-Fri</p>
-                        <h6>Emailreferrals@petermac.org</h6>
-                        <h6>Phone(03) 8559 5021</h6>
+                        <p>Business hours: Mon-Fri</p>
+                        <h6>travelsociety@info.org</h6>
+                        <h6>Phone:(035) 0098 1187</h6>
                     </div>
                 </Col>
 
                 <Col>
                     <div className=" p-3 rounded-3  contact-div">
                         <h4>SUPPORT OUR WORK</h4>
-                        <p>MediCare Foundation</p>
-                        <h6>Emailfoundation@petermac.org</h6>
-                        <h6> Phone1800 111 440</h6>
-                        <h6> Toll Free03) 8559 7777</h6>
+                        <p>Children Care Foundation</p>
+                        <h6>Childrenfoundation@care.org</h6>
+                        <h6> Phone: (077) 4434 5106</h6>
+                        <h6> Toll Free: (008) 6608 9730</h6>
                     </div>
                 </Col>
             </Row>
